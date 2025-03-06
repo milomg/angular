43f4c5b4db02e470bbe9c960d9cbd50b4dec3fd8
Milo <milomg4@gmail.com>
2025-03-06 18:56:18 +0000

    WIP

---
+++ devtools/projects/ng-devtools/src/lib/devtools-tabs/directive-explorer/signals-view/signals-tab.component.ts
@@ -0,0 +1,12 @@
+
+import {Component, input, output} from '@angular/core';
+import {DirectivePosition} from 'protocol';
+
+
+@Component({
+  templateUrl: './signals-tab.component.html',
+  selector: 'ng-signals-tab',
+  imports: [],
+})
+export class SignalsTabComponent {
+}
